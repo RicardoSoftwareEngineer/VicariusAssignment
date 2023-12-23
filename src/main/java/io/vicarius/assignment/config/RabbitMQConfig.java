5b02aebc13ce563df295c9cc0566b74511b226ba
Ricardo Ribeiro <riceirolves@gmail.com>
2023-12-23 01:43:32 -0300

    RabbitMQ configuration file added.

---
+++ src/main/java/io/vicarius/assignment/config/RabbitMQConfig.java
@@ -0,0 +1,52 @@
+package io.vicarius.assignment.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+@Configuration
+public class RabbitMQConfig {
+    public static final String QUEUE = "rabbit_mq_queue";
+    public static final String EXCHANGE = "rabbit_mq_exchange";
+    public static final String ROUTING_KEY = "rabbit_mq_routing_key";
+
+    @Bean
+    public Queue queue(){
+        return new Queue(QUEUE);
+    }
+
+    @Bean
+    public DirectExchange directExchange(){
+        return new DirectExchange(EXCHANGE);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, Exchange directExchange){
+        return BindingBuilder.bind(queue).to(directExchange).with(ROUTING_KEY).noargs();
+    }
+
+    @Bean
+    public MessageConverter messageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public AmqpTemplate getTemplate(ConnectionFactory connectionFactory){
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter());
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public Jackson2ObjectMapperBuilder objectMapperBuilder() {
+        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+        builder.serializationInclusion(JsonInclude.Include.NON_NULL);
+        return builder;
+    }
+}
