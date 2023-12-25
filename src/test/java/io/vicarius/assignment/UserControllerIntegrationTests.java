eca449d048be7c4c1f723a793451cc09faf00fa3
Ricardo Ribeiro <riceirolves@gmail.com>
2023-12-24 23:01:41 -0300

    Integration tests added for User controller

---
+++ src/test/java/io/vicarius/assignment/UserControllerIntegrationTests.java
@@ -0,0 +1,100 @@
+package io.vicarius.assignment;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.vicarius.assignment.user.UserDTO;
+import io.vicarius.assignment.user.UserServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+class UserControllerIntegrationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	ObjectMapper objectMapper;
+
+	@Autowired
+	private UserServiceImpl userService;
+
+	@Test
+	void testCreateProduct() throws Exception {
+		UserDTO userDTO = new UserDTO("Ricardo", "Ribeiro");
+		mockMvc.perform(post("/user/v1")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(objectMapper.writeValueAsString(userDTO)))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.id").isString())
+				.andExpect(jsonPath("$.firstName").value("Ricardo"))
+				.andExpect(jsonPath("$.lastName").value("Ribeiro"));
+	}
+
+	@Test
+	void testGetUserById() throws Exception {
+		UserDTO user = userService.create(new UserDTO("Ricardo", "Ribeiro"));
+		user = userService.create(user);
+
+		mockMvc.perform(get("/user/v1/{id}", user.getId()))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.id").value(user.getId()))
+				.andExpect(jsonPath("$.firstName").value("Ricardo"))
+				.andExpect(jsonPath("$.lastName").value("Ribeiro"));
+	}
+
+	@Test
+	void testGetUsers() throws Exception {
+		UserDTO user1 = userService.create(new UserDTO("Ricardo", "Ribeiro"));
+		userService.create(user1);
+
+		UserDTO user2 = userService.create(new UserDTO("Ricardo", "Rib"));
+		userService.create(user2);
+
+		mockMvc.perform(get("/user/v1"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$", hasSize(2)))
+				.andExpect(jsonPath("$[0].firstName").value("Ricardo"))
+				.andExpect(jsonPath("$[0].lastName").value("Ribeiro"))
+				.andExpect(jsonPath("$[1].firstName").value("Ricardo"))
+				.andExpect(jsonPath("$[1].lastName").value("Rib"));
+	}
+
+	@Test
+	void testUpdateUser() throws Exception {
+		UserDTO user = userService.create(new UserDTO("Ricardo", "Ribeiro"));
+		userService.create(user);
+		user.setFirstName("Ric");
+		user.setLastName("Rib");
+
+		mockMvc.perform(put("/user/v1/{id}", user.getId())
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(objectMapper.writeValueAsString(user)))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.id").value(user.getId()))
+				.andExpect(jsonPath("$.firstName").value("Ric"))
+				.andExpect(jsonPath("$.lastName").value("Rib"));
+	}
+
+	@Test
+	void testDeleteUser() throws Exception {
+		UserDTO user = userService.create(new UserDTO("Ricardo", "Ribeiro"));
+		userService.create(user);
+
+		mockMvc.perform(delete("/user/v1/{id}", user.getId()))
+				.andExpect(status().isOk());
+
+		user = userService.retrieveById(user.getId());
+		assertTrue(user != null);
+	}
+}
